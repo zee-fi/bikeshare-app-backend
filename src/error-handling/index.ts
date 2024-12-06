2f7c980acc10b49d71d859062c877257128aefab
fiona-eh <fionahulse@mac.com>
2024-12-06 17:13:14 +0100

    initial commit

---
+++ src/error-handling/index.ts
@@ -0,0 +1,23 @@
+import { Request, Response, NextFunction, ErrorRequestHandler, Application } from "express";
+
+module.exports = (app: Application) => {
+  app.use((req: Request, res: Response, next: NextFunction) => {
+    // this middleware runs whenever requested page is not available
+    res.status(404).json({ message: "This route does not exist" });
+  });
+
+  app.use((err: ErrorRequestHandler, req: Request, res: Response, next: NextFunction) => {
+    // whenever you call next(err), this middleware will handle the error
+    // always logs the error
+    console.error("ERROR", req.method, req.path, err);
+
+    // only render if the error ocurred before sending the response
+    if (!res.headersSent) {
+      res
+        .status(500)
+        .json({
+          message: "Internal server error. Check the server console",
+        });
+    }
+  });
+};
---
+++ src/routes/index.routes.ts
@@ -0,0 +1,9 @@
+import { Response, Request, NextFunction } from "express";
+
+const router = require("express").Router();
+
+router.get("/", (req: Request, res: Response, next: NextFunction) => {
+  res.json("All good in here");
+});
+
+module.exports = router;
